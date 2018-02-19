2004b943446e0220bd651008340d267563012696
Sami Emad <samiemad.me@gmail.com>
2018-02-19 01:11:23 +0100

    initial commit

---
+++ run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+g++ vid.cpp --std=c++14 -O3 -o vid || exit -1
+
+if [[ -z $1 ]]; then
+	files="example";
+else
+	files="me_at_the_zoo videos_worth_spreading trending_today";
+#	files="me_at_the_zoo videos_worth_spreading trending_today kittens";
+fi
+
+#for f in input/*.in; do
+#	name=${f#*/}
+#	name=${name%.*}
+for name in $files; do
+	printf "Running for file $name...";
+	time ./vid < "input/${name}.in" > "output/${name}.out" ;
+	score=`./score "output/$name.out" < "input/$name.in"` || exit -1
+	echo "Your score for $name is: $score"
+	total=$(python -c "print $total+$score")
+#	echo "file $name processing done";
+done
+
+echo "Total score: $total"
---
+++ vid.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int V,E,R,C,X;
+int v[10004];
+int t[1003];
+vector<pair<int,int>> adj[1003];
+vector<pair<int,int>> rdj[1003];
+int rv[1000006];
+int re[1000006];
+int rn[1000006];
+
+vector<tuple<double, int, int, int>> w;
+int sz[1003];
+vector<int> sol[1003];
+set<pair<int,int>> u;
+
+int main(){
+	cin>>V>>E>>R>>C>>X;
+	for(int i=0; i<V; ++i){
+		cin>>v[i];
+	}
+	for(int i=0; i<E; ++i){
+		int K;
+		cin>>t[i]>>K;
+		for(int j=0; j<K; ++j){
+			int x,l;
+			cin>>x>>l;
+			adj[i].push_back({x,l});
+			rdj[x].push_back({i,l});
+		}
+	}
+	for(int i=0; i<R; ++i){
+		cin>>rv[i]>>re[i]>>rn[i];
+	}
+
+	for(int i=0; i<R; ++i){
+		int e = re[i];
+		for(auto p : adj[e]){
+			int c = p.first;
+			int l = p.second;
+			double imp = t[e] - l;
+			imp *= 1;
+			imp *= rn[i] * 1;
+			imp /= v[rv[i]] * 1;
+			w.push_back( make_tuple(imp, rv[i], re[i], c) );
+		}
+	}
+	sort(w.begin(), w.end());
+//	reverse(w.begin(), w.end());
+	cerr<<w.size()<<"\n"<<get<0>(w[0])<<" "<<get<0>(w[w.size()-1])<<"\n";
+	for(auto x:w){
+		double imp;
+		int vv, ee, cc;
+		tie(imp, vv, ee, cc) = x;
+		if( sz[ee] + v[vv] <= X && !u.count({ee,vv}) ){
+			sz[ee] += v[vv];
+			sol[ee].push_back(vv);
+			u.insert({ee,vv});
+		}
+	}
+	cout<<C<<"\n";
+	for(int i=0; i<C; ++i){
+//		if(sol[i].empty()) continue;
+		cout<<i;
+		for(auto x:sol[i]){
+			cout<<" "<<x;
+		}
+		cout<<"\n";
+	}
+	return 0;
+}
